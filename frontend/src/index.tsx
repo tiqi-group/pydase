e7350c9ec03b2347c22a0762a0d9768e31f1a62d
Mose Müller <mosemueller@gmail.com>
2023-08-02 12:06:19 +0200

    frontend: adding Bootstrap CSS

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Render the App component into the #root div
 const container = document.getElementById('root');
 const root = createRoot(container);
