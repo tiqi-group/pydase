cbdf894073c8b11690bc317cc1471db21e911299
Mose Müller <mosemueller@gmail.com>
2023-08-02 12:06:21 +0200

    fixing warning messages comming from asyncio

--- src/pyDataInterface/utils/warnings.py
+++ src/pyDataInterface/utils/warnings.py
@@ -5,10 +5,11 @@ def warn_if_instance_class_does_not_inherit_from_DataService(__value: object) ->
     base_class_name = __value.__class__.__base__.__name__
     module_name = __value.__class__.__module__
 
-    if module_name not in ["builtins", "__builtin__"] and base_class_name not in [
-        "DataService",
-        "list",
-    ]:
+    if module_name not in [
+        "builtins",
+        "__builtin__",
+        "asyncio.unix_events",
+    ] and base_class_name not in ["DataService", "list", "Enum"]:
         logger.warning(
             f"Warning: Class {type(__value).__name__} does not inherit from DataService."
         )
