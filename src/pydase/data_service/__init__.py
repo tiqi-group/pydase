b317cba95fb8c4ab710758ed71b9140f1f40cb9a
Mose Müller <mosemueller@gmail.com>
2023-08-02 15:30:45 +0200

    fix flake8 error

--- src/pydase/data_service/__init__.py
+++ src/pydase/data_service/__init__.py
@@ -1,9 +1,7 @@
-from ..components.number_slider import NumberSlider
 from .data_service import DataService
 from .data_service_list import DataServiceList
 
 __all__ = [
     "DataService",
     "DataServiceList",
-    "NumberSlider",
 ]
