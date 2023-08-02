919f83c77ee05c44e18a30001cdea0abcf089924
Mose Müller <mosemueller@gmail.com>
2023-08-02 12:06:19 +0200

    updating from react 17 -> 18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createRoot } from 'react-dom/client';
 
 // Render the App component into the #root div
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
