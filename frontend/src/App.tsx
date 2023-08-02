d3ad419c3350ed7f5eb4f82d87f3664a65f2b03d
Mose Müller <mosemueller@gmail.com>
2023-08-02 12:06:19 +0200

    feat: adding frontend code (react)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// A simple functional component
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Welcome to my React application. This is some test.</p>
+    </div>
+  );
+};
+
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+// Render the App component into the #root div
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
