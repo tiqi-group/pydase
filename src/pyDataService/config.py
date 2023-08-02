1f4c3db9bb68c6898301957a23f9c285dc181693
Mose Müller <mosemueller@gmail.com>
2023-08-02 12:06:22 +0200

    updating config.py

--- src/pyDataService/config.py
+++ src/pyDataService/config.py
@@ -1,9 +1,6 @@
-from pathlib import Path
 from typing import Literal
 
 from confz import ConfZ, ConfZEnvSource
-
-CONFIG_DIR = Path(__file__).parent.parent.parent.resolve() / "config"
 
 
 class OperationMode(ConfZ):  # type: ignore
