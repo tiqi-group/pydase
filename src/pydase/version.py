b1dcceebf7e920f02854065bf161a8aff67cb7b9
Mose Müller <mosemueller@gmail.com>
2023-08-08 14:12:46 +0200

    fix: version number is not read from pyproject.toml anymore

--- src/pydase/version.py
+++ src/pydase/version.py
@@ -1,5 +1,2 @@
-import toml
-
-pyproject = toml.load("pyproject.toml")
-__version__ = pyproject["tool"]["poetry"]["version"]
+__version__ = "0.1.0"
 __major__, __minor__, __patch__ = [int(v) for v in __version__.split(".")]
