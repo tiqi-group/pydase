b8b503b3d6b94df16ad4f6583de09082c7905b99
Mose Müller <mosemueller@gmail.com>
2023-08-08 14:15:35 +0200

    feat: adding version test

---
+++ tests/test_version.py
@@ -0,0 +1,9 @@
+import toml
+
+import pydase.version
+
+
+def test_project_version() -> None:
+    pyproject = toml.load("pyproject.toml")
+    pydase_pyroject_version = pyproject["tool"]["poetry"]["version"]
+    assert pydase.version.__version__ == pydase_pyroject_version
