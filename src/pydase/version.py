a2ce2e1116bd6cc8949f928fc29bb294d8e8f774
Mose Müller <mosemueller@gmail.com>
2023-08-17 15:47:42 +0200

    update version.py to use metadata

--- src/pydase/version.py
+++ src/pydase/version.py
@@ -1,2 +1,4 @@
-__version__ = "0.1.0"
+from importlib.metadata import distribution
+
+__version__ = distribution("pydase").version
 __major__, __minor__, __patch__ = [int(v) for v in __version__.split(".")]
