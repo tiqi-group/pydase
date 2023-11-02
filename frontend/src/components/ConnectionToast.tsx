c4056d3ca8d5ff4a311899ee231342877c264b89
Mose Müller <mosemueller@gmail.com>
2023-11-02 15:31:46 +0100

    chore: formatting, renaming

--- frontend/src/components/ConnectionToast.tsx
+++ frontend/src/components/ConnectionToast.tsx
@@ -1,69 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { Toast, Button, ToastContainer } from 'react-bootstrap';
 
-type SnackbarToast = {
+type ConnectionToastProps = {
   connectionStatus: string;
 };
 
-export const ConnectionToast = React.memo(({ connectionStatus }: SnackbarToast) => {
-  const [show, setShow] = useState(true);
+export const ConnectionToast = React.memo(
+  ({ connectionStatus }: ConnectionToastProps) => {
+    const [show, setShow] = useState(true);
 
-  useEffect(() => {
-    setShow(true);
-  }, [connectionStatus]);
+    useEffect(() => {
+      setShow(true);
+    }, [connectionStatus]);
 
-  const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false);
 
-  const getToastContent = (): {
-    message: string;
-    bg: string; // bootstrap uses `bg` prop for background color
-    delay: number | undefined;
-  } => {
-    switch (connectionStatus) {
-      case 'connecting':
-        return {
-          message: 'Connecting...',
-          bg: 'info',
-          delay: undefined
-        };
-      case 'connected':
-        return { message: 'Connected', bg: 'success', delay: 1000 };
-      case 'disconnected':
-        return {
-          message: 'Disconnected',
-          bg: 'danger',
-          delay: undefined
-        };
-      case 'reconnecting':
-        return {
-          message: 'Reconnecting...',
-          bg: 'info',
-          delay: undefined
-        };
-      default:
-        return {
-          message: '',
-          bg: 'info',
-          delay: undefined
-        };
-    }
-  };
+    const getToastContent = (): {
+      message: string;
+      bg: string; // bootstrap uses `bg` prop for background color
+      delay: number | undefined;
+    } => {
+      switch (connectionStatus) {
+        case 'connecting':
+          return {
+            message: 'Connecting...',
+            bg: 'info',
+            delay: undefined
+          };
+        case 'connected':
+          return { message: 'Connected', bg: 'success', delay: 1000 };
+        case 'disconnected':
+          return {
+            message: 'Disconnected',
+            bg: 'danger',
+            delay: undefined
+          };
+        case 'reconnecting':
+          return {
+            message: 'Reconnecting...',
+            bg: 'info',
+            delay: undefined
+          };
+        default:
+          return {
+            message: '',
+            bg: 'info',
+            delay: undefined
+          };
+      }
+    };
 
-  const { message, bg, delay } = getToastContent();
+    const { message, bg, delay } = getToastContent();
 
-  return (
-    <ToastContainer position="bottom-center">
-      <Toast
-        show={show}
-        onClose={handleClose}
-        delay={delay}
-        autohide={delay !== undefined}
-        bg={bg}>
-        <Toast.Body className="d-flex justify-content-between">
-          {message}
-          <Button variant="close" size="sm" onClick={handleClose} />
-        </Toast.Body>
-      </Toast>
-    </ToastContainer>
-  );
-});
+    return (
+      <ToastContainer position="bottom-center">
+        <Toast
+          show={show}
+          onClose={handleClose}
+          delay={delay}
+          autohide={delay !== undefined}
+          bg={bg}>
+          <Toast.Body className="d-flex justify-content-between">
+            {message}
+            <Button variant="close" size="sm" onClick={handleClose} />
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
+    );
+  }
+);
