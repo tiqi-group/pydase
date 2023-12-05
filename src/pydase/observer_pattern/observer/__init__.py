f97cd7eb4e7e2901b698b225426614dcdf58ac5f
Mose Müller <mosemueller@gmail.com>
2023-12-05 12:48:46 +0100

    adds observers to observer namespace

---
+++ src/pydase/observer_pattern/observer/__init__.py
@@ -0,0 +1,7 @@
+from pydase.observer_pattern.observer.observer import Observer
+from pydase.observer_pattern.observer.property_observer import PropertyObserver
+
+__all__ = [
+    "Observer",
+    "PropertyObserver",
+]
