e3eaf5ffe29eaa998bab2e3a9a01b50a1da65584
Mose Müller <mosemueller@gmail.com>
2023-12-19 16:11:32 +0100

    adds ServiceConfig and WebServerConfig

--- src/pydase/config.py
+++ src/pydase/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Literal
 
 from confz import BaseConfig, EnvSource
@@ -7,3 +8,15 @@ class OperationMode(BaseConfig):  # type: ignore[misc]
     environment: Literal["development", "production"] = "development"
 
     CONFIG_SOURCES = EnvSource(allow=["ENVIRONMENT"])
+
+
+class ServiceConfig(BaseConfig):  # type: ignore[misc]
+    service_config_dir: Path = Path("config")
+
+    CONFIG_SOURCES = EnvSource(allow=["SERVICE_CONFIG_DIR"])
+
+
+class WebServerConfig(BaseConfig):  # type: ignore[misc]
+    generate_new_web_settings: bool = False
+
+    CONFIG_SOURCES = EnvSource(allow=["GENERATE_NEW_WEB_SETTINGS"])
