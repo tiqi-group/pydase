464478cda9c0b1d0696c99eea16787f80dd6a352
Mose Müller <mosemueller@gmail.com>
2023-12-20 10:14:07 +0100

    removes helper function to create config folder

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -1,9 +1,6 @@
-import tempfile
-from pathlib import Path
 
 import pytest
 from pydase.utils.helpers import (
-    create_config_folder,
     is_property_attribute,
 )
 
@@ -32,20 +29,3 @@ def test_is_property_attribute(attr_name: str, expected: bool) -> None:
 
     dummy = DummyClass()
     assert is_property_attribute(dummy, attr_name) == expected
-
-
-def test_create_config_folder() -> None:
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        print("created temporary directory", tmpdirname)
-
-        config_dir = Path(tmpdirname) / "config"
-
-        assert not config_dir.exists()
-
-        create_config_folder(config_dir)
-
-        assert config_dir.exists()
-
-        # Call the function again to test idempotency (it should not fail if the
-        # directory already exists)
-        create_config_folder(config_dir)
