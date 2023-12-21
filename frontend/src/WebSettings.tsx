9fc6d6f9102b2649c58aaef48da551f6ae640c85
Mose Müller <mosemueller@gmail.com>
2023-12-21 10:03:56 +0100

    updates WebSettings.tsx

--- frontend/src/WebSettings.tsx
+++ frontend/src/WebSettings.tsx
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
-export const WebSettingsContext = createContext({});
+export const WebSettingsContext = createContext<Record<string, WebSetting>>({});
 
 export type WebSetting = {
   displayName: string;
   index: number;
-}[];
+};
