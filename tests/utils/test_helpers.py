6cae76bde108047d913daa4ed3a858a2a1e886e6
Mose Müller <mosemueller@gmail.com>
2024-04-22 19:10:50 +0200

    adds tests for parse_keyed_attribute

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -1,7 +1,9 @@
+from typing import Any
 
 import pytest
 from pydase.utils.helpers import (
     is_property_attribute,
+    parse_keyed_attribute,
 )
 
 
@@ -29,3 +31,18 @@ def test_is_property_attribute(attr_name: str, expected: bool) -> None:
 
     dummy = DummyClass()
     assert is_property_attribute(dummy, attr_name) == expected
+
+
+@pytest.mark.parametrize(
+    "attr_name, expected",
+    [
+        ("attr_name", ("attr_name", None)),
+        ("list_attr[2]", ("list_attr", 2)),
+        ('dict_attr["2"]', ("dict_attr", "2")),
+        ('dict_attr["some_key"]', ("dict_attr", "some_key")),
+        ("dict_attr[2]", ("dict_attr", 2)),
+        ("dict_attr[2.1]", ("dict_attr", 2.1)),
+    ],
+)
+def test_parse_keyed_attributes(attr_name: str, expected: tuple[str, Any]) -> None:
+    assert parse_keyed_attribute(attr_name) == expected
