fa45ee566ba4b434e12b5b8071045cc87517592d
Mose Müller <mosemueller@gmail.com>
2024-07-08 15:16:13 +0200

    fixes eslint error

--- frontend/src/components/MethodComponent.tsx
+++ frontend/src/components/MethodComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { runMethod } from "../socket";
 import { Button, Form } from "react-bootstrap";
 import { DocStringComponent } from "./DocStringComponent";
