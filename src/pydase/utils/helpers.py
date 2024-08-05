456090fee9ada93aafd1a2e076b007d520b129d9
Mose Müller <mosemueller@gmail.com>
2024-08-05 16:41:11 +0200

    adds is_descriptor helper method

--- src/pydase/utils/helpers.py
+++ src/pydase/utils/helpers.py
@@ -196,3 +196,7 @@ def function_has_arguments(func: Callable[..., Any]) -> bool:
 
     # Check if there are any parameters left which would indicate additional arguments.
     return len(parameters) > 0
+
+def is_descriptor(obj):
+    """Check if an object is a descriptor."""
+    return any(hasattr(obj, method) for method in ("__get__", "__set__", "__delete__"))
