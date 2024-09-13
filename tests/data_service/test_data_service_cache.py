2e31ebb7d947aef854b31cae65bfd96d1aff4a8e
Mose Müller <mosemueller@gmail.com>
2024-09-13 12:59:37 +0200

    fixes or removes task-related tests

--- tests/data_service/test_data_service_cache.py
+++ tests/data_service/test_data_service_cache.py
@@ -1,7 +1,6 @@
 import logging
 
 import pydase
-import pytest
 from pydase.data_service.data_service_observer import DataServiceObserver
 from pydase.data_service.state_manager import StateManager
 
@@ -32,36 +31,4 @@ def test_nested_attributes_cache_callback() -> None:
             "value"
         ]
         == "Ciao"
-    )
-
-
-@pytest.mark.asyncio(scope="function")
-async def test_task_status_update() -> None:
-    class ServiceClass(pydase.DataService):
-        name = "World"
-
-        async def my_method(self) -> None:
-            pass
-
-    service_instance = ServiceClass()
-    state_manager = StateManager(service_instance)
-    DataServiceObserver(state_manager)
-
-    assert (
-        state_manager.cache_manager.get_value_dict_from_cache("my_method")["type"]
-        == "method"
-    )
-    assert (
-        state_manager.cache_manager.get_value_dict_from_cache("my_method")["value"]
-        is None
-    )
-
-    service_instance.start_my_method()  # type: ignore
-    assert (
-        state_manager.cache_manager.get_value_dict_from_cache("my_method")["type"]
-        == "method"
-    )
-    assert (
-        state_manager.cache_manager.get_value_dict_from_cache("my_method")["value"]
-        == "RUNNING"
     )
